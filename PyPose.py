2cea9ffcdf802ed5a65e2f765bbd6c8ea65a3586
Alexander Krause <alexander.krause@erazor-zone.de>
2013-09-26 22:07:19 +0200

    new connection dialog added, support for different drivers added

--- PyPose.py
+++ PyPose.py
@@ -25,7 +25,21 @@ import wx
 import serial
 
 from ax12 import *
-from drivers.serial import Driver
+
+try:
+    from drivers.drv_serial import Driver as serial_Driver
+    HAS_DRIVER_SERIAL=True
+except Exception:
+    HAS_DRIVER_SERIAL=False
+    print("Serial driver not supported!")
+
+try:
+    from drivers.dynamixel_zmq import Driver as dynamixel_zmq_Driver
+    HAS_DRIVER_DZMQ=True
+except Exception:
+    HAS_DRIVER_DZMQ=False
+    print("Driver dynamixel_zmq not supported!")
+
 
 from PoseEditor import *
 from SeqEditor import *
@@ -44,7 +58,8 @@ class editor(wx.Frame):
     ID_EXIT=wx.NewId()
     ID_EXPORT=wx.NewId()
     ID_RELAX=wx.NewId()
-    ID_PORT=wx.NewId()
+    ID_CONNECTION=wx.NewId()
+    ID_CONNECT=wx.NewId()
     ID_ABOUT=wx.NewId()
     ID_TEST=wx.NewId()
     ID_TIMER=wx.NewId()
@@ -64,7 +79,7 @@ class editor(wx.Frame):
         self.toolIndex = dict() # existant tools
         self.saveReq = False
         self.panel = None
-        self.port = None
+        self.driver = None
         self.filename = ""
         self.dirname = ""
         self.columns = 2        # column count for pose editor
@@ -72,9 +87,11 @@ class editor(wx.Frame):
         # for clearing red color on status bar
         self.timer = wx.Timer(self, self.ID_TIMER)
         self.timeout = 0
+
+        self.connected = False
         
         # build our menu bar  
-        menubar = wx.MenuBar()
+        self.menubar = wx.MenuBar()
         prjmenu = wx.Menu()
         prjmenu.Append(self.ID_NEW, "new") # dialog with name, # of servos
         prjmenu.Append(self.ID_OPEN, "open") # open file dialog
@@ -82,7 +99,7 @@ class editor(wx.Frame):
         prjmenu.Append(self.ID_SAVE_AS,"save as") # ask for name, save
         prjmenu.AppendSeparator()
         prjmenu.Append(self.ID_EXIT,"exit") 
-        menubar.Append(prjmenu, "project")
+        self.menubar.Append(prjmenu, "project")
 
         toolsmenu = wx.Menu()
         # find our tools
@@ -98,25 +115,27 @@ class editor(wx.Frame):
             self.toolIndex[cid] = (t, name)
             toolsmenu.Append(cid,name)   
         toolsmenu.Append(self.ID_EXPORT,"export to AVR") # save as dialog
-        menubar.Append(toolsmenu,"tools")
+        self.menubar.Append(toolsmenu,"tools")
 
-        configmenu = wx.Menu()
-        configmenu.Append(self.ID_PORT,"port") # dialog box: arbotix/thru, speed, port
-        columnmenu = wx.Menu()        
-        columnmenu.Append(self.ID_2COL,"2 columns")
-        columnmenu.Append(self.ID_3COL,"3 columns")
-        columnmenu.Append(self.ID_4COL,"4 columns")
-        configmenu.AppendMenu(self.ID_COL_MENU,"pose editor",columnmenu)
+        self.menu_config = wx.Menu()
+        self.menu_config.Append(self.ID_CONNECTION,"connection-setup") # dialog box: connection
+        self.menu_config.Append(self.ID_CONNECT,"connect")
+
+        self.menu_column = wx.Menu()        
+        self.menu_column.Append(self.ID_2COL,"2 columns")
+        self.menu_column.Append(self.ID_3COL,"3 columns")
+        self.menu_column.Append(self.ID_4COL,"4 columns")
+        self.menu_config.AppendMenu(self.ID_COL_MENU,"pose editor",self.menu_column)
         # live update
-        self.live = configmenu.Append(self.ID_LIVE_UPDATE,"live pose update",kind=wx.ITEM_CHECK)
-        #configmenu.Append(self.ID_TEST,"test") # for in-house testing of boards
-        menubar.Append(configmenu, "config")    
+        self.live = self.menu_config.Append(self.ID_LIVE_UPDATE,"live pose update",kind=wx.ITEM_CHECK)
+        #menu_config.Append(self.ID_TEST,"test") # for in-house testing of boards
+        self.menubar.Append(self.menu_config, "config")    
 
-        helpmenu = wx.Menu()
-        helpmenu.Append(self.ID_ABOUT,"about")
-        menubar.Append(helpmenu,"help")
+        self.menu_help = wx.Menu()
+        self.menu_help.Append(self.ID_ABOUT,"about")
+        self.menubar.Append(self.menu_help,"help")
 
-        self.SetMenuBar(menubar)    
+        self.SetMenuBar(self.menubar)    
 
         # configure events
         wx.EVT_MENU(self, self.ID_NEW, self.newFile)
@@ -130,7 +149,8 @@ class editor(wx.Frame):
         wx.EVT_MENU(self, self.ID_EXPORT, self.export)     
 
         wx.EVT_MENU(self, self.ID_RELAX, self.doRelax)   
-        wx.EVT_MENU(self, self.ID_PORT, self.doPort)
+        wx.EVT_MENU(self, self.ID_CONNECTION, self.showConnectionDialog)
+        wx.EVT_MENU(self, self.ID_CONNECT, self.toggleConnection)
         wx.EVT_MENU(self, self.ID_TEST, self.doTest)
         wx.EVT_MENU(self, self.ID_ABOUT, self.doAbout)
         self.Bind(wx.EVT_CLOSE, self.doClose)
@@ -172,7 +192,7 @@ class editor(wx.Frame):
         # instantiate
         module = __import__(t, globals(), locals(), [t,"STATUS"])
         panelClass = getattr(module, t)
-        self.panel = panelClass(self,self.port)
+        self.panel = panelClass(self,self.driver)
         self.sizer=wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.panel,1,wx.EXPAND|wx.ALL,10)
         self.SetSizer(self.sizer)
@@ -275,44 +295,79 @@ class editor(wx.Frame):
                     except:
                         pass
         return self.ports
-    def doPort(self, e=None):
-        """ open a serial port """
-        if self.port == None:
-            self.findPorts()
-        dlg = wx.SingleChoiceDialog(self,'Port (Ex. COM4 or /dev/ttyUSB0)','Select Communications Port',self.ports)
-        #dlg = PortDialog(self,'Select Communications Port',self.ports)
-        if dlg.ShowModal() == wx.ID_OK:
-            if self.port != None:
-                self.port.ser.close()
-            print("Opening port: " + self.ports[dlg.GetSelection()])
-            self.openPort(self.ports[dlg.GetSelection()])
-            dlg.Destroy()
-            
-    def openPort(self, port, baud=38400, interpolate=True):
+			
+    def doConnect(self):
+        driver_type=self.project.connection['type']
+        error=False
         try:
-            # TODO: add ability to select type of driver
-            self.port = Driver(port, baud, interpolate)
-            self.panel.port = self.port
-            self.panel.portUpdated()
-            self.sb.SetStatusText(port + '@' + str(baud),1)
-        except:
-            self.port = None
+            if driver_type == 'serial':
+                if not HAS_DRIVER_SERIAL:
+                    print("Connection type not supported!")
+                    error=True
+                else:
+                    con_port=self.project.connection['settings']['serial']['port']
+                    con_baudrate=self.project.connection['settings']['serial']['baudrate']
+                    self.driver = serial_Driver(
+                        con_port,
+                        con_baudrate,
+                        True
+                    )
+
+                    status_text="%s @ %i"%(con_port,con_baudrate)
+            elif driver_type == 'dynamixel_zmq':
+                if not HAS_DRIVER_DZMQ:
+                    print("Connection type not supported!")
+                    error=True
+                
+
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            exception_str="\n".join('!! ' + line for line in lines)
+            print(exception_str)
+            error=True
+        if error:
+            self.driver = None
             self.sb.SetBackgroundColour('RED')
-            self.sb.SetStatusText('Could Not Open Port ' + port,0)
+            self.sb.SetStatusText('Could not connect with driver %s'%driver_type,0)
             self.sb.SetStatusText('not connected',1)
             self.timer.Start(20)
-        return self.port
+        else:
+            self.connected=True
+            self.menu_config.SetLabel(self.ID_CONNECT,'disconnect')
+            self.panel.port = self.driver
+            self.panel.portUpdated()
+            self.sb.SetStatusText(status_text,1)
+            
+    def doDisconnect(self):
+        if self.project.connection['type'] == 'serial':
+            if self.driver:
+                self.driver.close()
+                self.driver=None
+        self.connected=False
+        self.menu_config.SetLabel(self.ID_CONNECT,'connect')
+
+    def toggleConnection(self,e=None):
+        if not self.connected:
+            self.doConnect()
+        else:
+            self.doDisconnect()
+        print("connect-toggle")
+        
+    def showConnectionDialog(self, e=None):
+        conn_dlg=ConnectionSetup(self,-1)
+        conn_dlg.Show(True)
         
     def doTest(self, e=None):
-        if self.port != None:
-            self.port.execute(253, 25, list())
+        if self.driver != None:
+            self.driver.execute(253, 25, list())
 
     def doRelax(self, e=None):
         """ Relax servos so you can pose them. """
-        if self.port != None:
+        if self.driver != None:
             print("PyPose: relaxing servos...")      
             for servo in range(self.project.count):
-                self.port.setReg(servo+1,P_TORQUE_ENABLE, [0,])    
+                self.driver.setReg(servo+1,P_TORQUE_ENABLE, [0,])    
         else:
             self.sb.SetBackgroundColour('RED')
             self.sb.SetStatusText("No Port Open",0) 
@@ -410,7 +465,140 @@ class NewProjectDialog(wx.Dialog):
 
         self.SetSizer(vbox)
 
+# Connection Dialog
+class ConnectionSetup(wx.Frame):
+    
+    def __init__(self, *args, **kwds):
+        kwds["style"] = wx.FRAME_FLOAT_ON_PARENT
+        self.parent=args[0]
+        wx.Frame.__init__(self, *args, **kwds)
+        self.Centre()
+        self.con_types={
+            0:'serial',
+            1:'dynamixel_zmq'
+        }
+        self.baudrates=["9600", "19200", "38400", "57600", "115200", "1000000", "2000000"]
 
+        self.con_type = wx.Notebook(self, -1, style=0)
+        
+        #page for serial
+        self.con_pane_serial = wx.Panel(self.con_type, -1)
+        self.label_port = wx.StaticText(self.con_pane_serial, -1, "Port")
+        self.combo_port = wx.ComboBox(self.con_pane_serial, -1, choices=[], style=wx.CB_DROPDOWN)
+        self.label_baudrate = wx.StaticText(self.con_pane_serial, -1, "Baudrate")
+        self.combo_baudrate = wx.ComboBox(self.con_pane_serial, -1, choices=self.baudrates, style=wx.CB_DROPDOWN)
+
+        #page for dynamixel_zmq
+        self.con_pane_dzmq = wx.Panel(self.con_type, -1)
+        self.label_uri = wx.StaticText(self.con_pane_dzmq, -1, "URI")
+        self.combo_uri = wx.ComboBox(self.con_pane_dzmq, -1, choices=["tcp://localhost:5555", "ipc:///var/run/dynamixel_zmq"], style=wx.CB_DROPDOWN)
+
+        self.button_cancel = wx.Button(self, -1, "Cancel")
+        self.button_ok = wx.Button(self, -1, "Ok")
+
+        self.__set_properties()
+        self.__do_layout()
+
+        self.Bind(wx.EVT_BUTTON, self.doCancel, self.button_cancel)
+        self.Bind(wx.EVT_BUTTON, self.doOK, self.button_ok)
+
+    def __set_properties(self):
+        self.SetTitle("Connection")
+        connection_setup=self.parent.project.connection
+        """
+        for type_id in self.con_types:
+            if connection_setup['type'] == self.con_types[type_id]:
+                self.con_type.ChangeSelection(int(type_id))
+        """
+        
+        port_list=self.parent.findPorts()
+        self.combo_port.Clear()
+        #fill serial_pane
+        for port in port_list:
+            self.combo_port.Append(port)
+        if (not 'settings' in connection_setup) or (type(connection_setup['settings'])!=dict):
+            connection_setup['settings']={}
+        connection_settings=connection_setup['settings']
+        if ('serial' in connection_settings):
+            if ('port' in connection_settings['serial']) and (type(connection_settings['serial']['port'])==str):
+                self.combo_port.SetValue(connection_settings['serial']['port'])
+        
+            if ('baudrate' in connection_settings['serial']) and (type(connection_settings['serial']['baudrate'])==int):
+                self.combo_baudrate.SetValue(str(connection_settings['serial']['baudrate']))
+        
+        #fill dzmq_pane
+        if ('dynamixel_zmq' in connection_settings):
+            self.combo_uri.Clear()
+            if (not 'uris' in connection_settings['dynamixel_zmq']) or (type(connection_settings['dynamixel_zmq']['uris'])!=list):
+                connection_settings['dynamixel_zmq']['uris']=[]
+            else:
+                for uri in connection_settings['dynamixel_zmq']['uris']:
+                    self.combo_uri.Append(uri)
+            self.combo_uri.SetValue(connection_settings['dynamixel_zmq']['uri'])
+
+    def __do_layout(self):
+        sizer_5 = wx.BoxSizer(wx.VERTICAL)
+        grid_sizer_2 = wx.GridSizer(1, 2, 0, 0)
+        grid_sizer_4 = wx.GridSizer(3, 2, 0, 0)
+
+        grid_sizer_3 = wx.GridSizer(3, 2, 0, 0)
+        grid_sizer_3.Add(self.label_port, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_3.Add(self.combo_port, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_3.Add(self.label_baudrate, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_3.Add(self.combo_baudrate, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        self.con_pane_serial.SetSizer(grid_sizer_3)
+        self.con_type.AddPage(self.con_pane_serial, "Serial")
+
+        grid_sizer_4.Add(self.label_uri, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_4.Add(self.combo_uri, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        self.con_pane_dzmq.SetSizer(grid_sizer_4)
+        self.con_type.AddPage(self.con_pane_dzmq, "Dynamixel-ZMQ")
+
+        sizer_5.Add(self.con_type, 1, wx.EXPAND, 0)
+        grid_sizer_2.Add(self.button_cancel, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        grid_sizer_2.Add(self.button_ok, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+        sizer_5.Add(grid_sizer_2, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer_5)
+        sizer_5.Fit(self)
+        self.Layout()
+        # end wxGlade
+
+    def doCancel(self, event):
+        event.Skip()
+        self.Destroy()
+
+    def doOK(self, event):
+        if not 'settings' in self.parent.project.connection:
+            self.parent.project.connection['settings']={}
+            
+        new_con_type=self.con_types[self.con_type.GetSelection()]
+        #some kind of a soft-pointer / don't get confused ;-)
+        settings_p= self.parent.project.connection['settings']
+        self.parent.project.connection['type']= new_con_type
+        if new_con_type == 'serial':
+            if not 'serial' in settings_p:
+                settings_p['serial']={}
+             
+            settings_p['serial']['port']=str(self.combo_port.GetValue())
+            settings_p['serial']['baudrate']=int(self.combo_baudrate.GetValue())
+        elif new_con_type == 'dynamixel_zmq':
+            if not 'dynamixel_zmq' in settings_p:
+                settings_p['dynamixel_zmq']={}
+            new_dzmq_uri=str(self.combo_uri.GetValue())
+            settings_p['dynamixel_zmq']['uri']=new_dzmq_uri
+            if not 'uris' in settings_p['dynamixel_zmq']:
+               settings_p['dynamixel_zmq']['uris']=[]
+            
+            if (not new_dzmq_uri in settings_p['dynamixel_zmq']['uris']):
+                settings_p['dynamixel_zmq']['uris'].append(new_dzmq_uri)         
+                #max 10 of last uri's
+                if len(settings_p['dynamixel_zmq']['uris'])>10:
+                    settings_p['dynamixel_zmq']['uris']=settings_p['dynamixel_zmq']['uris'][1:]
+        #print(settings_p)
+        self.parent.project.save = True
+        event.Skip()
+        self.Destroy()
+        
 if __name__ == "__main__":
     print("PyPose starting... ")
     app = wx.App()
